eb6a2b10a228ce79356688061cf1d7131c22de20
Allan-Sanchez <asanchezrixtun@gmail.com>
2020-11-06 08:31:34 -0600

    Diana reservations

---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+export default{
+    // apiKey: "AIzaSyBWs9FSORxukwrxTFzLGIZi20M4Y9azacQ",
+    // authDomain: "reservacionescanchasintetica.firebaseapp.com",
+    // databaseURL: "https://reservacionescanchasintetica.firebaseio.com",
+    // projectId: "reservacionescanchasintetica",
+    // storageBucket: "reservacionescanchasintetica.appspot.com",
+    // messagingSenderId: "286208971294",
+    // appId: "1:286208971294:web:0f1f0d9845dc10385b940f"
+
+    apiKey: "AIzaSyA0UFjTvncRcZqSDyWAvnOkbkpP2DpqArU",
+    authDomain: "diana-reservaciones.firebaseapp.com",
+    databaseURL: "https://diana-reservaciones.firebaseio.com",
+    projectId: "diana-reservaciones",
+    storageBucket: "diana-reservaciones.appspot.com",
+    messagingSenderId: "1075708107688",
+    appId: "1:1075708107688:web:829fc9b7cc4e279871db9d"
+
+};
